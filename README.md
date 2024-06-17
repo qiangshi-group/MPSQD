45cddd7b3bf0a31233117590b9ca427d2fbf6b92
ssrstyss <43660849+ssrstyss@users.noreply.github.com>
2024-06-17 19:52:45 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+Mpsqd is a python package that used matrix product states (MPS) to solve both the time dependent Schroding equation (TDSE) and the HEOM.
+
+### Installation
+pip install mpsqd
+
+### Usage
+Two modes of using the code are provided. 
+- The first one is provided in the /examples, where the user provides necessary python files for initial conditions, construnction of the MPO, while use the package as a library to to the time depenent propagation
+- The second approach uses the pre-built models, where the models are presented in the /models directory, examples for this type of usage in provided in the /ipynb diectory.
+
+### Documentation
+Primitive documentation can be found in the included .ipynb files
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mpsqd',
+    version='0.1.0',    
+    description='A Python package based on MPS to solve the TDSE and HEOM',
+    url='https://github.com/',
+    author='Qiang Shi',
+    author_email='a@b.c.d',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'scipy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
