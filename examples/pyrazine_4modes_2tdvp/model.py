4346e69493032c425d4af05baa6801abde0ad040
ssrstyss <43660849+ssrstyss@users.noreply.github.com>
2024-09-01 14:35:00 +0800

    Add files via upload

--- examples/pyrazine_4modes_2tdvp/model.py
+++ examples/pyrazine_4modes_2tdvp/model.py
@@ -28,10 +28,6 @@ omega[3] = 0.1525  # 9a 0.1525 0.1564 0.1673  # w9a   = 0.1525  ,ev
 # this is the unit transform
 omega = omega/au2ev
 #=================================================
-# A and B terms 
-#acoef = np.zeros(4,dtype=np.float64)
-#bcoef = np.zeros(4,dtype=np.float64)
-#ccoef = np.zeros(4,dtype=np.float64)
 coef1 = np.zeros((nstate,nstate,nmode),dtype=np.float64)
 coef2 = np.zeros((nstate,nstate,nmode,nmode),dtype=np.float64)
 
@@ -44,20 +40,12 @@ coef1[1,1,1] = -0.13545  #kl_6a_s2  =  -0.13545 , ev
 coef1[1,1,2] = 0.171     #kl_1_s2  =   0.17100 , ev
 coef1[1,1,3] = 0.03746   #kl_9a_s2  =   0.03746 , ev
                      
-#acoef = acoef/au2ev #!*au2a
-#bcoef = bcoef/au2ev #!*au2a
 #================================================
-# the first order C terms, only for 10a 
                              # linear, off-diagonal coupling coefficients
 coef1[0,1,0] = 0.20804           #lambda =   0.20804 , ev
-#ccoef = ccoef/au2ev 
 coef1 = coef1/au2ev
 
 #=================================================
-# second order A and B terms
-#a2coef = np.zeros((4,4),dtype=np.float64)
-#b2coef = np.zeros((4,4),dtype=np.float64)
-
                                # quadratic, on-diagonal coupling coefficients
                                # H(1,1)
 coef2[0,0,0,0] = -0.01159         # kq_10a_s1  =  -0.01159 , ev
@@ -80,18 +68,11 @@ coef2[1,1,1,2] = -0.00298        # kb_6ax1_s2  =  -0.00298 , ev
 coef2[1,1,2,3] = -0.00155        # kb_1x9a_s2  =  -0.00155 , ev
 coef2[1,1,1,3] =  0.00189       # kb_6ax9a_s2  =   0.00189 , ev
 
-#a2coef = a2coef/au2ev  #!*au2a**2
-#b2coef = b2coef/au2ev  #!*au2a**2
-
 #==================================================
-# the C terms
-#c2coef = np.zeros((4,4),dtype=np.float64)
                           # bilinear, off-diagonal coupling coefficients
                           # H(1,2) and H(2,1)
 coef2[0,1,0,2] =  0.00553     #kb_1x10a  =   0.00553 , ev
 coef2[0,1,0,1] =  0.01000    #kb_6ax10a  =  0.01000 , ev
 coef2[0,1,0,3] =  0.00126     #kb_9ax10a  =   0.00126 , ev
-
-#c2coef = c2coef/au2ev #*au2a**2
 
 coef2 = coef2/au2ev
