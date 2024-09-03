6ea1100c939201ad96634a36b0be50bafc7ec537
ssrstyss <43660849+ssrstyss@users.noreply.github.com>
2024-09-03 19:00:58 +0800

    Add files via upload

--- examples/holstein/params.py
+++ examples/holstein/params.py
@@ -4,40 +4,45 @@ import numpy as np
 # constants
 au2fs = 0.02418884254
 small = 1e-16
-mmax = 8
-nrtt = 20
-nrmax = 20
-nsteps = 50000
+mmax = 15
+nrtt = 90
+nrmax = 40
+nsteps = 20000
 
 #model parameters
-eta = 0.5
-gamma = 1.0
 beta = 1.0
-vcoupling = 1.0
+vcoupling = 0.1
 eps = 0.0
 
-omega = gamma
+nmbo = 1
+pk = []
+omega_b =[]
+gamma_b = [0.8]
+for i in range(nmbo):
+  omega_b.append(np.sqrt(1.0**2 - gamma_b[i]**2))
+  pk.append(omega_b[i]**2*gamma_b[i] + gamma_b[i]**3)
 delta = vcoupling
-dt = 0.001
+dt = 0.005
 
-nl1    = 5
-nsite  = 7
+nl1    = 3
+nsite  = 21
+nsiteeq = 1
 ndvr   = nsite
+ndvreq = nsiteeq
 nlevel = nsite*nl1
-nb1    = 10
+nleveleq = nsiteeq*nl1
+nb1    = 20
 nb2    = nb1
-
-# read/write pall
-write_pall = False
-
-# paramaters
-
 nmps = nlevel+2
-nb = np.zeros(nlevel+2,dtype=int)
+
+nb = np.zeros(nmps,dtype=int)
 nb[0] = ndvr
 nb[1] = nb1
 nb[2:nlevel+1] = nb2
 nb[nlevel+1] = ndvr
+
+# read/write pall
+write_pall = False
 
 nbmat = nb**2
 
@@ -48,29 +53,39 @@ def setupfreqs():
   gval_i = np.empty(nl1,dtype=np.complex128)
 
   gval_i[:] = 0.0
-  gval[0] = eta*omega/2.0/np.tan(0.5*beta*omega)
-  gval_i[0] = -eta*omega/2.0
-  wval[0] = omega
-
-  for i in range(1,nl1,1):
-      omegatmp = 2.0*np.pi*i/beta
-      gval[i] = -2.0*eta*omega/beta*omegatmp/(omega**2-omegatmp**2)
-      wval[i] = omegatmp
-
-  k0 = (eta/beta-gval[0])/omega
-
-  for i in range(2,nl1+1,1):
-      k0 = k0-gval[i-1]/wval[i-1]
-
+  for i in range(nmbo):
+    pk1 = pk[i]/8.0/gamma_b[i]/omega_b[i]
+    wval[2*i]   = gamma_b[i] + 1.0j*omega_b[i]
+    wval[2*i+1] = gamma_b[i] - 1.0j*omega_b[i]
+    wtmp1 = 0.5*beta*(-1.0j*gamma_b[i] + omega_b[i])
+    wtmp2 = 0.5*beta*(+1.0j*gamma_b[i] + omega_b[i])
+    gval[2*i]   = pk1*(np.exp(wtmp1) + np.exp(-wtmp1))/(np.exp(wtmp1) - np.exp(-wtmp1))
+    gval[2*i+1] = pk1*(np.exp(wtmp2) + np.exp(-wtmp2))/(np.exp(wtmp2) - np.exp(-wtmp2))
+    gval_i[2*i] = -1.0j*pk1
+    gval_i[2*i+1] = 1.0j*pk1
+  for i in range(1,nl1-2*nmbo+1,1):
+    omegatmp = 2.0*np.pi*i/beta
+    jwtmp = 0.0
+    for j in range(nmbo):
+      jwtmp += pk[j]/((-omegatmp**2+omega_b[j]**2+gamma_b[j]**2)**2+4*omega_b[j]**2*omegatmp**2)
+    gval[i+2*nmbo-1] = -2.0*jwtmp/beta*omegatmp
+    wval[i+2*nmbo-1] = omegatmp
+  k0 = 0.0
+  for i in range(nl1-2*nmbo+1,10000,1):
+    omegatmp = 2.0*np.pi*i/beta
+    jwtmp = 0.0
+    for j in range(nmbo):
+      jwtmp += pk[j]/((-omegatmp**2+omega_b[j]**2+gamma_b[j]**2)**2+4*omega_b[j]**2*omegatmp**2)
+    k0 -= 2.0*jwtmp/beta
   a0coef = 0.0
   normfac = np.empty(nl1,dtype=np.float128)
   for i in range(1,nl1+1,1):
-      normfac[i-1] = np.sqrt(abs(gval[i-1]))
-      a0coef += np.real(gval[i-1])
+     normfac[i-1] = np.sqrt(abs(gval[i-1]))
+     a0coef += np.real(gval[i-1])
 
   normfac = np.sqrt(a0coef)*np.ones(nl1)
-  return wval,gval,gval_i,k0,normfac
 
+  return wval, gval, gval_i, k0, normfac
 wval,gval,gval_i,k0,normfac = setupfreqs()
 
 print("ndvr =", ndvr)
